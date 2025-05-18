9870902f06d8194a66c9a74ea7ac3336a2a47fda
CrimsonDeck <crimsondeckgames@gmail.com>
2025-05-18 08:44:56 -0400

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <CrimsonLib.h>
+
+int main() {
+    print("--- Basic read function ---");
+    int num;
+    print("Enter an integer: ");
+    read(num);
+    print("You entered:", num);
+}
